6f13e460edfcad28d7001bd2ea29f2d4f365aaf6
Назин Аркадий <arkadii.nazin@simbirsoft.com>
2021-07-23 23:50:57 +0300

    scaffold for twitter-to-kafka-service

---
+++ twitter-to-kafka-service/src/test/java/io/arcanrun/twittertokafka/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package  io.arcanrun.twittertokafka;
+
+import io.arcanrun.twittertokafka.TwitterToKafkaServiceApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class TwitterToKafkaServiceApplicationTests {
+
+    @Test
+    public void testWillBeHere(){
+    }
+}
